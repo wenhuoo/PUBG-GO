0944b96eb519369fbfeaed3abd7a3f48d4e30b13
Owen <owu@anziif.com>
2018-09-24 15:17:04 +1000

    updated click to close collapse

--- JavaScript/jq.js
+++ JavaScript/jq.js
@@ -1,1 +1,7 @@
 $('body').scrollspy({ target: '#navbar-site' })
+
+$(document).click(function(e) {
+	if (!$(e.target).is('.container-fluid')) {
+    	$('.collapse').collapse('hide');	    
+    }
+});
